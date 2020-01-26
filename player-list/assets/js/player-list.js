d30528548debedcb727628e8dd0732ee9a37ab11
Asnxthaony <asnxthaony@outlook.com>
2020-01-27 07:29:29 +0800

    [player-list] Truncate too long username

--- player-list/assets/js/player-list.js
+++ player-list/assets/js/player-list.js
@@ -3,7 +3,7 @@ fetch('https://s1-api.mcstaralliance.com/onlinePlayers')
     .then(data => {
         document.querySelector('#player-list').innerHTML = '';
 
-        for(var i in data) {
+        for (var i in data) {
             var name = data[i];
             var color = 'black';
 
@@ -17,14 +17,19 @@ fetch('https://s1-api.mcstaralliance.com/onlinePlayers')
                 color = 'orange';
             }
 
+            displayName = name;
+            if  (name.length >= 12) {
+                displayName = name.slice(0, 9) + '...'
+            }
+
             document.querySelector('#player-list').innerHTML += `
                 <div class="col-12 col-sm-12 col-md-4">
                     <div class="info-box">
                         <span class="info-box-icon bg-info elevation-1">
-                            <img src="avatar/player/45/` + name + `.png" alt="` + name + `" width="45" height="45">
+                            <img src="avatar/player/` + name + `" alt="` + name + `" width="45" height="45">
                         </span>
                         <div class="info-box-content">
-                            <span class="info-box-text text-` + color + `">` + name + `</span>
+                            <span class="info-box-text text-` + color + `" title="` + name + `">` + displayName + `</span>
                             <span class="info-box-number">
                                 ❤
                             </span>
