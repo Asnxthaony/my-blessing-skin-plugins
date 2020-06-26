eb7bd5ee7128b5cac415f8b1b0c45a949375313d
Asnxthaony <asnxthaony@outlook.com>
2020-06-26 17:39:14 +0800

    [mcstaralliance] add crossorigin attr

--- mcstaralliance/assets/js/waifu-tips.js
+++ mcstaralliance/assets/js/waifu-tips.js
@@ -194,6 +194,7 @@ function loadExternalResource(url, type) {
         } else if (type === 'js') {
             elem = document.createElement('script');
             elem.src = url;
+            elem.crossorigin = 'anonymous';
         }
 
         if (elem) {
@@ -206,8 +207,8 @@ function loadExternalResource(url, type) {
 
 if (screen.width >= 768) {
     Promise.all([
-        loadExternalResource('https://skin.mcstaralliance.com/plugins/mcstaralliance/assets/css/waifu.css?v=1.0.2', 'css'),
-        loadExternalResource('https://cdn.jsdelivr.net/gh/stevenjoezhang/live2d-widget@master/live2d.min.js?v1.0.2', 'js'),
+        loadExternalResource('https://skin.mcstaralliance.com/plugins/mcstaralliance/assets/css/waifu.css?v=1.0.3', 'css'),
+        loadExternalResource('https://cdn.jsdelivr.net/gh/stevenjoezhang/live2d-widget@master/live2d.min.js?v=1.0.3', 'js'),
     ]).then(() => {
         if (localStorage.getItem('waifu-display') && Date.now() - localStorage.getItem('waifu-display') <= 86400000) {
             // 已关闭
