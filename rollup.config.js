53fb045f1951511f045c65a070a43a85016db13e
Asnxthaony <asnxthaony@outlook.com>
2020-09-30 23:20:51 +0800

    webpack -> Rollup

---
+++ rollup.config.js
@@ -0,0 +1,60 @@
+// @ts-check
+import glob from 'fast-glob'
+import typescript from '@rollup/plugin-typescript'
+import replace from '@rollup/plugin-replace'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import { terser } from 'rollup-plugin-terser'
+import svelte from 'rollup-plugin-svelte'
+import sveltePreprocess from 'svelte-preprocess'
+
+const isDev = process.env.NODE_ENV === 'development'
+
+/**
+ * @param {string} name
+ * @param {string} path
+ *
+ * @returns {import('rollup').RollupOptions}
+ */
+function makeConfig(name, path) {
+  return {
+    input: { [name]: path },
+    output: {
+      dir: './plugins',
+      format: 'iife',
+      globals: {
+        'blessing-skin': 'blessing',
+        react: 'React',
+        'react-dom': 'ReactDOM',
+      },
+      indent: '  ',
+      sourcemap: isDev,
+    },
+    external: ['react', 'react-dom', 'blessing-skin'],
+    plugins: [
+      replace({
+        'process.env.NODE_ENV': isDev ? '"development"' : '"production"',
+      }),
+      typescript(),
+      svelte({
+        preprocess: sveltePreprocess(),
+      }),
+      resolve({ browser: true }),
+      commonjs(),
+      !isDev && terser(),
+    ],
+  }
+}
+
+export default glob([
+  'plugins/*/assets/**/*.ts',
+  'plugins/*/assets/**/*.tsx',
+  '!plugins/*/assets/**/*.test.ts',
+  '!**/*.d.ts',
+]).then((files) =>
+  files.map((file) => {
+    const name = file.replace('plugins/', '').replace(/\.tsx?$/g, '')
+
+    return makeConfig(name, file)
+  }),
+)
